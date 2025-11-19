1a719d004040ea07102eebb5b2230084597a6a46
suuxxirr <102400242+suuxxirr@users.noreply.github.com>
2025-11-19 17:59:33 +0900

    feat: 회원가입에 검증 기능 추가

--- src/main/java/com/playlist_shop/controller/UserController.java
+++ src/main/java/com/playlist_shop/controller/UserController.java
@@ -2,9 +2,13 @@ package com.playlist_shop.controller;
 
 import com.playlist_shop.dto.UserJoinRequestDto;
 import com.playlist_shop.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -19,13 +23,19 @@ public class UserController {
     }
 
     @GetMapping("/join")
-    public String joinPage() {
+    public String joinPage(Model model) {
+
+        model.addAttribute("userJoinRequestDto", new UserJoinRequestDto());
         return "joinForm";
     }
 
     @PostMapping("/join")
-    public String joinProcess(UserJoinRequestDto requestDto){
-        userService.join(requestDto);
-        return "redirect:/";
+    public String joinProcess(@Valid @ModelAttribute UserJoinRequestDto userJoinRequestDto, BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()) {
+            return "joinForm";
+        }
+        userService.join(userJoinRequestDto);
+        return "redirect:/login";
     }
 }
--- src/main/java/com/playlist_shop/dto/UserJoinRequestDto.java
+++ src/main/java/com/playlist_shop/dto/UserJoinRequestDto.java
@@ -3,6 +3,9 @@ package com.playlist_shop.dto;
 
 
 import com.playlist_shop.domain.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +15,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserJoinRequestDto {
+    @NotBlank
+    @Size(min = 2, max =10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
     private String nickname;
+
+    @NotBlank
+    @Size(min = 8, message = "비밀번호는 8자 이상으로 입력해주세요.")
     private String password;
+
+    @NotBlank
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String mail;
 
 }
