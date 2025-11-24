38327e2298096591e6ec50fde18cfcd784504230
suuxxirr <102400242+suuxxirr@users.noreply.github.com>
2025-11-24 16:28:15 +0900

    refactor: like 테스트 리팩토링
    - user 생성, 저장 메소드 분리
    - song 생성, 저장 메소드 분리

--- src/test/java/com/playlist_shop/service/SongLikeServiceTest.java
+++ src/test/java/com/playlist_shop/service/SongLikeServiceTest.java
@@ -36,23 +36,8 @@ class SongLikeServiceTest {
     @DisplayName("종아요 누르기 & 취소하기 성공")
     void like_success() {
         // given
-        UserJoinRequestDto requestDto = new UserJoinRequestDto();
-        requestDto.setNickname("testUser");
-        requestDto.setPassword("test123!");
-        requestDto.setMail("test@example.com");
-
-        Song song = Song.builder()
-                .title("Test Song")
-                .artist("Test Artist")
-                .price(4900)
-                .album("Test EP")
-                .releaseDate(LocalDate.of(2022, 8, 1))
-                .albumartUrl("Test.jpg")
-                .build();
-
-        Long savedUserId = userService.join(requestDto);
-        User user = userRepository.findById(savedUserId).orElseThrow();
-        songRepository.save(song);
+        User user = makeAndSaveUser();
+        Song song = makeAndSaveSong();
 
         // when 1 (좋아요 누르기)
         songLikeService.actionLike(user, song.getId());
@@ -65,6 +50,30 @@ class SongLikeServiceTest {
         assertThat(songLikeRepository.findByUserAndSong(user, song)).isEmpty();
 
 
+    }
+
+    private User makeAndSaveUser() {
+        UserJoinRequestDto requestDto = new UserJoinRequestDto();
+        requestDto.setNickname("testUser");
+        requestDto.setPassword("test123!");
+        requestDto.setMail("test@example.com");
+
+        Long savedUserId = userService.join(requestDto);
+        return userRepository.findById(savedUserId).orElseThrow();
+
+    }
+
+    private Song makeAndSaveSong() {
+        Song song = Song.builder()
+                .title("Test Song")
+                .artist("Test Artist")
+                .price(4900)
+                .album("Test EP")
+                .releaseDate(LocalDate.of(2022, 8, 1))
+                .albumartUrl("Test.jpg")
+                .build();
+        songRepository.save(song);
+        return song;
     }
 
 }
