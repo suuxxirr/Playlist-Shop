140cd696902378af812aebb008742595041c0d49
suuxxirr <102400242+suuxxirr@users.noreply.github.com>
2025-11-26 22:04:40 +0900

    feat: CartController에 장바구니 페이지 연결 메소드 추가

--- src/main/java/com/playlist_shop/controller/CartController.java
+++ src/main/java/com/playlist_shop/controller/CartController.java
@@ -1,5 +1,7 @@
 package com.playlist_shop.controller;
 
+import com.playlist_shop.domain.Cart;
+import com.playlist_shop.domain.CartSong;
 import com.playlist_shop.domain.User;
 import com.playlist_shop.repository.UserRepository;
 import com.playlist_shop.service.CartService;
@@ -7,8 +9,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,5 +36,19 @@ public class CartController {
         cartService.addCart(user, songId);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/cart")
+    public String cartPage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        if (userDetails == null) {
+            return "redirect:/login";
+        }
+
+        User user = userRepository.findByNickname(userDetails.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다"));
+
+        List<CartSong> cartSongs = cartService.userCartView(user);
+        model.addAttribute("cartList", cartSongs);
+        return "cartList";
     }
 }
