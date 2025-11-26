f4cdc6c93e69ba4504fa7ee606fcd93cedba2bdc
suuxxirr <102400242+suuxxirr@users.noreply.github.com>
2025-11-26 21:56:54 +0900

    feat: CartService 목록 조회 기능 추가
    - CartService에 유저의 장바구니에 담긴 노래 목록을 가져오는 메소드 추가

--- src/main/java/com/playlist_shop/repository/CartSongRepository.java
+++ src/main/java/com/playlist_shop/repository/CartSongRepository.java
@@ -6,8 +6,11 @@ import com.playlist_shop.domain.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 public interface CartSongRepository extends JpaRepository<CartSong, Long> {
     Optional<CartSong> findByCartAndSong(Cart cart, Song song);
+
+    List<CartSong> findByCart(Cart cart);
 }
--- src/main/java/com/playlist_shop/service/CartService.java
+++ src/main/java/com/playlist_shop/service/CartService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -41,5 +42,13 @@ public class CartService {
 
         cartSongRepository.save(cartSong);
 
+    }
+
+    @Transactional
+    public List<CartSong> userCartView(User user) {
+        Cart cart = cartRepository.findByUser(user);
+        List<CartSong> cartSongs = cartSongRepository.findByCart(cart);
+
+        return cartSongs;
     }
 }
