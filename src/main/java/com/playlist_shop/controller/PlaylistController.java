7dd12c3b983ca06a147687bbcb2404400a099f1c
suuxxirr <102400242+suuxxirr@users.noreply.github.com>
2025-12-18 16:45:46 +0900

    feat: playlist 상세보기 기능 추가
    - playlist 상세 보기
    - header 적용

--- src/main/java/com/playlist_shop/controller/PlaylistController.java
+++ src/main/java/com/playlist_shop/controller/PlaylistController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -31,5 +32,12 @@ public class PlaylistController {
         List<Playlist> playlists = playlistService.findAllPlaylists();
         model.addAttribute("playlists", playlists);
         return "playlistList";
+    }
+
+    @GetMapping("/playlist/{id}")
+    public String playlistDetail(@PathVariable Long id, Model model) {
+        Playlist playlist = playlistService.getPlaylist(id);
+        model.addAttribute("playlist", playlist);
+        return "playlistDetail";
     }
 }
