ec3f70718682c581bc074a56f9f7326b575400c7
suuxxirr <102400242+suuxxirr@users.noreply.github.com>
2025-12-20 17:39:46 +0900

    feat: like 버튼 클릭 시 이동 페이지 수정

--- src/main/java/com/playlist_shop/controller/LikeController.java
+++ src/main/java/com/playlist_shop/controller/LikeController.java
@@ -4,12 +4,14 @@ import com.playlist_shop.domain.User;
 import com.playlist_shop.repository.SongLikeRepository;
 import com.playlist_shop.repository.UserRepository;
 import com.playlist_shop.service.SongLikeService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -22,7 +24,8 @@ public class LikeController {
 
     @GetMapping("/like/{songId}")
     public String like(@PathVariable Long songId,
-                        @AuthenticationPrincipal UserDetails userDetails) {
+                       @AuthenticationPrincipal UserDetails userDetails,
+                       HttpServletRequest request) {
 
         if (userDetails == null) {
             return "redirect:/login";
@@ -33,6 +36,11 @@ public class LikeController {
 
         songLikeService.actionLike(user, songId);
 
-        return "redirect:/";
+        String previousPage = request.getHeader("Referer");
+        if (previousPage != null & !previousPage.isBlank()) {
+            return "redirect:" + previousPage;
+        } else {
+            return "redirect:/";
+        }
     }
 }
